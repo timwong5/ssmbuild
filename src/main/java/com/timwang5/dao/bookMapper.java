fe54a979f2e82d6ff81862b1cde3007be789df33
Yining Wang <wangyining203@gmail.com>
2022-05-30 16:15:11 +0800

    finish the ssm structure

---
+++ src/main/java/com/timwang5/dao/bookMapper.java
@@ -0,0 +1,23 @@
+package com.timwang5.dao;
+
+import com.timwang5.pojo.Books;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface bookMapper {
+    //增加一个Book
+    int addBook(Books book);
+
+    //根据id删除一个Book
+    int deleteBookById(@Param("bookId") int id);
+
+    //更新Book
+    int updateBook(Books books);
+
+    //根据id查询,返回一个Book
+    Books queryBookById(int id);
+
+    //查询全部Book,返回list集合
+    List<Books> queryAllBook();
+}
---
+++ src/main/java/com/timwang5/service/bookServiceImpl.java
@@ -0,0 +1,41 @@
+package com.timwang5.service;
+
+import com.timwang5.dao.bookMapper;
+import com.timwang5.pojo.Books;
+
+import java.util.List;
+
+public class bookServiceImpl implements bookService{
+
+    //Service²ãµ÷Dao²ã
+    private bookMapper bookMapper;
+
+    public void setBookMapper(com.timwang5.dao.bookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    @Override
+    public int addBook(Books book) {
+        return bookMapper.addBook(book);
+    }
+
+    @Override
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    @Override
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    @Override
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    @Override
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
