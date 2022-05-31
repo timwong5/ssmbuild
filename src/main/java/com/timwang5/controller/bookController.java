3f5a9c04eabd335458a85b5e1baae2a0ad270ef5
Yining Wang <wangyining203@gmail.com>
2022-06-01 00:56:41 +0800

    finish this demo

---
+++ src/main/java/com/timwang5/controller/bookController.java
@@ -0,0 +1,71 @@
+package com.timwang5.controller;
+
+
+import com.timwang5.pojo.Books;
+import com.timwang5.service.bookService;
+import com.timwang5.service.bookServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class bookController {
+
+    @Autowired
+    @Qualifier("bookServiceImpl")
+    private bookService bookService = new bookServiceImpl();
+
+    @RequestMapping("/allBook")
+    public String list(Model model) {
+        List<Books> list = bookService.queryAllBook();
+        model.addAttribute("list", list);
+        return "allBook";
+    }
+
+    //跳转到增加书籍界面
+    @RequestMapping("/toAddBook")
+    public String toAddpage(){
+        return "addBook";
+    }
+
+    @RequestMapping("/addBook")
+    public String addPaper(Books books) {
+        System.out.println(books);
+        bookService.addBook(books);
+        //重定向到allBook请求
+        return "redirect:/book/allBook";
+    }
+
+
+    @RequestMapping("/toUpdateBook")
+    public String toUpdateBook(Model model, int id) {
+        Books books = bookService.queryBookById(id);
+        System.out.println(books);
+        model.addAttribute("book",books );
+        return "updateBook";
+    }
+
+    @RequestMapping("/updateBook")
+    public String updateBook(Model model, Books book) {
+        System.out.println(book);
+        bookService.updateBook(book);
+        Books books = bookService.queryBookById(book.getBookID());
+        model.addAttribute("books", books);
+        return "redirect:/book/allBook";
+    }
+
+
+    @RequestMapping("/del/{bookId}")
+    public String deleteBook(@PathVariable("bookId") int id) {
+        bookService.deleteBookById(id);
+        return "redirect:/book/allBook";
+    }
+
+
+}
